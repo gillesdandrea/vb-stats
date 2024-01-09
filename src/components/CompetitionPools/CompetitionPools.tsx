84428fdef749cbc86c17cac56fa2ef9047bd38aa
Gilles d'Andrea <gillesdandrea@users.noreply.github.com>
2024-01-09 11:46:10 +0100

    Updated tour 5, small ux fixes

--- src/components/CompetitionPools/CompetitionPools.tsx
+++ src/components/CompetitionPools/CompetitionPools.tsx
@@ -218,7 +218,7 @@ const CompetitionPools = ({ competition, day, singleDay, qualified, className }:
     return () => {
       debouncedSearchHandler.cancel();
     };
-  }, []);
+  });
 
   const cday = competition.days[day];
   if (!cday) {
