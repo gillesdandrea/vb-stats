7bebbebe7df3a8e6a64f66810af4fe6ee6d9dcfe
Gilles d'Andrea <gillesdandrea@users.noreply.github.com>
2024-03-13 16:09:55 +0100

    Add stylelint order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,7 @@
 /** @type {import('stylelint').Config} */
 const config = {
-  "extends": [
-    "stylelint-config-standard-scss"
-  ]
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-clean-order/error'],
+  plugins: ['stylelint-order'],
 };
 
 export default config;
