079733dc4830203fafcc94bf3bb0f256b2fede74
Gilles d'Andrea <gillesdandrea@users.noreply.github.com>
2024-10-24 15:25:37 +0200

    Updated M18M Day02
    Updated M18M Day02

--- src/app/Shell/Shell.tsx
+++ src/app/Shell/Shell.tsx
@@ -49,7 +49,7 @@ const parseQueryParameters = (url: string): Record<string, string> => {
   const params: Record<string, string> = {};
   let match;
   while ((match = regex.exec(url))) {
-    params[match[1]] = match[2];
+    params[match[1]] = decodeURIComponent(match[2]);
   }
   return params;
 };
